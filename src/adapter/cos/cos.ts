e7fd4eb2e41cb24a530d5966d97a1c59ab78dd23
blessyou <370732889@qq.com>
2021-05-06 19:35:31 +0800

    refactor: update oss

--- src/adapter/cos/cos.ts
+++ src/adapter/cos/cos.ts
@@ -7,34 +7,13 @@ const cos = new COS({
   SecretKey: process.env.COS_SECRET_KEY || "",
 });
 
-const QiNiuUpLoader = (
-  key: string | null,
-  rsStream: NodeJS.ReadableStream,
-  putExtra: QiNiu.form_up.PutExtra | null
-) => {
+export const GetQiNiuUpLoaderToken = (key: string | null) => {
   const c = new QiNiu.auth.digest.Mac();
   const config = {
     scope: "unliar",
   };
   const uploadToken = new QiNiu.rs.PutPolicy(config).uploadToken(c);
-
-  return new Promise((resolve, reject) => {
-    new QiNiu.form_up.FormUploader().putStream(
-      uploadToken,
-      key,
-      rsStream,
-      putExtra,
-      (err, resp, respInfo) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve({
-          ...resp,
-          ...respInfo,
-        });
-      }
-    );
-  });
+  return uploadToken;
 };
 
 export default cos;
